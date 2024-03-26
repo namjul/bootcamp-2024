1b6676dcb850c2cd61d3a3247aa95cd4399887e2
Samuel Hobl <samuel.hobl@gmail.com>
2024-03-26 17:14:31 +0100

    add mod 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
-import 'dotenv/config'
+import "dotenv/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
+  redirects: {
+    "/app": "/app/dashboard",
+  },
 });
